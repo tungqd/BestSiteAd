c34804a7b03fc1123ddc1376eb1e65674c13865f
Tung Dang <tungqd90@yahoo.com>
2013-11-18 00:05:33 -0800

    minor update

--- SiteTest/config/config.php
+++ SiteTest/config/config.php
@@ -10,7 +10,6 @@
     DEFINE('GETAD_URL',PATH."get-ad/?format=");
     DEFINE('INCRE_URL',PATH."increment-choice/?adID=");
     DEFINE('INCRE_VUL',PATH."increment-vulnerable/?sql=");
-    DEFINE('SQL', 'update Counter set count = 1000');
     
     DEFINE('FORMAT', 'xml');
 ?>
