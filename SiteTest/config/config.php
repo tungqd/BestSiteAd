d49a9d84b7d601ace75dd79afdc0a68ecbaba6aa
khanhnguyen38 <katenguyenvn@Khanhs-MacBook-Pro.local>
2013-11-18 15:28:02 -0800

    path fixed

--- SiteTest/config/config.php
+++ SiteTest/config/config.php
@@ -5,7 +5,7 @@
     DEFINE('DATABASE', 'hw4_sitetest');
     DEFINE('SITENAME', 'SiteTest');
     
-    DEFINE('PATH',"http://localhost/CS174/Hw4/BestSiteAd/index.php/");
+    DEFINE('PATH',"http://localhost/Hw4/BestSiteAd/index.php/");
     
     DEFINE('GETAD_URL',PATH."get-ad/?format=");
     DEFINE('INCRE_URL',PATH."increment-choice/?adID=");
