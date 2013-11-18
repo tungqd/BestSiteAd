8dc9a1d1316148fd6ddf54b03fba4194bb0f0e89
Tung Dang <tungqd90@yahoo.com>
2013-11-17 21:24:25 -0800

    proxy works for json too!

--- SiteTest/config/config.php
+++ SiteTest/config/config.php
@@ -4,7 +4,7 @@
     DEFINE('PASSWORD', '');
     DEFINE('DATABASE', 'hw4_sitetest');
     DEFINE('SITENAME', 'SiteTest');
-    DEFINE('FORMAT', 'xml');
+    DEFINE('FORMAT', 'json');
     DEFINE('GETAD_URL',"http://localhost/CS174/Hw4/BestSiteAd/index.php/get-ad/?format=");
     DEFINE('INCRE_URL',"http://localhost/CS174/Hw4/BestSiteAd/index.php/increment-choice/?adID=");
 ?>
