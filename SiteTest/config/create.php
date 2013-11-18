7a544699ee558c8e605558d1a5d9ee5b9a4f6fa6
Tung Dang <tungqd90@yahoo.com>
2013-11-17 17:53:48 -0800

    some ads

--- SiteTest/config/create.php
+++ SiteTest/config/create.php
@@ -12,17 +12,17 @@
     $query .= "CREATE DATABASE $database;";
     $query .= "USE $database;";
     $query .= "DROP TABLE IF EXISTS News;";
-    $query .= "CREATE TABLE News (ID INT not null auto_increment,title VARCHAR(30) not null,content VARCHAR(200) not null, PRIMARY KEY(ID));";
-    $query .= "INSERT INTO News(title,content) VALUES ('First Story', 'This is our first story');";
-    $query .= "INSERT INTO News(title,content) VALUES ('Second Story', 'This is our second story');";
-    $query .= "INSERT INTO News(title,content) VALUES ('Third Story', 'This is our third story');";
-    $query .= "INSERT INTO News(title,content) VALUES ('Fourth Story', 'This is our fourth story');";
-    $query .= "INSERT INTO News(title,content) VALUES ('Fifth Story', 'This is our fifth story');";
-    $query .= "INSERT INTO News(title,content) VALUES ('Sixth Story', 'This is our sixth story');";
-    $query .= "INSERT INTO News(title,content) VALUES ('Seventh Story', 'This is our seventh story');";
-    $query .= "INSERT INTO News(title,content) VALUES ('Eighth Story', 'This is our eighth story');";
-    $query .= "INSERT INTO News(title,content) VALUES ('Ninth Story', 'This is our ninth story');";
-    $query .= "INSERT INTO News(title,content) VALUES ('Tenth Story', 'This is our tenth story');";
+    $query .= "CREATE TABLE News (ID INT not null auto_increment,title VARCHAR(30) not null, url VARCHAR(150) not null, content VARCHAR(300) not null, PRIMARY KEY(ID));";
+    $query .= "INSERT INTO News(title,url,content) VALUES ('Yoga Tablet', 'http://www.engadget.com/2013/11/17/the-yoga-tablet-does-kickstands-with-a-twist/', 'If one takes a narrow view of the tablet market.');";
+    $query .= "INSERT INTO News(title,url,content) VALUES ('Playstation4', 'http://www.engadget.com/2013/11/17/sony-sells-over-1-million-playstation-4-consoles-in-first-day/', 'Sony sells over 1 million PlayStation 4 console.');";
+    $query .= "INSERT INTO News(title,url,content) VALUES ('Tablo', 'http://www.engadget.com/2013/11/16/tablo-hands-on/', 'a DVR that streams over-the-air TV nearly anywhere');";
+    $query .= "INSERT INTO News(title,url,content) VALUES ('Quantum Computing', 'http://www.engadget.com/2013/11/16/oxford-university-scientists-break-qubit-survival-record-are-on/', 'Experiment brings quantum computing a step closer to reality');";
+    $query .= "INSERT INTO News(title,url,content) VALUES ('Twitter', 'http://www.engadget.com/2013/11/16/twitter-alpha-beta-android-app-design/', 'Twitter scraps Android app redesign previewed to testers');";
+    $query .= "INSERT INTO News(title,url,content) VALUES ('LG Flex', 'Twitter scraps Android app redesign previewed to testers, concocts a hybrid interface', 'LG G Flex appears on the FCC with AT&T-friendly LTE');";
+    $query .= "INSERT INTO News(title,url,content) VALUES ('London', 'http://www.engadget.com/2013/11/15/london-city-tld/', 'London becomes the latest city to get its own top-level domain');";
+    $query .= "INSERT INTO News(title,url,content) VALUES ('Adobe Reader', 'http://www.engadget.com/2013/11/15/adobe-reader-pdf-conversion-android/', 'Update for Android adds costly PDF conversion features');";
+    $query .= "INSERT INTO News(title,url,content) VALUES ('Galaxy Gear', 'http://www.engadget.com/2013/11/15/almost-all-of-your-notifications-can-now-display-on-galaxy-gear/','Almost all of your notifications can now display.');";
+    $query .= "INSERT INTO News(title,url,content) VALUES ('Microsoft', 'http://www.engadget.com/2013/11/15/steve-ballmer-explains-retirement-decision/', 'Steve Ballmer couldnt change Microsoft fast enough');";
 
     if (mysqli_multi_query($db,$query)) {
         echo "Database and Schemas for $database created successfully.";
